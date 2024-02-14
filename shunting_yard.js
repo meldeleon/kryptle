f72cb9cbbf771ab05b91cf010639fe97d646f8a0
meldeleon <melissadeleon@gmail.com>
2024-02-13 23:16:00 -0800

    finishing shunting yard algo

--- shunting_yard.js
+++ shunting_yard.js
@@ -21,59 +21,83 @@ const operators = {
   },
 }
 
+function assert(predicate) {
+  if (predicate) return
+  throw new Error("Assertion failed due to invalid token")
+}
+
 function shuntingYard(expr) {
+  const opSymbols = Object.keys(operators)
   const stack = []
   let output = ""
-  for (let symbol of expr) {
-    if (symbol === " ") continue
+  //helper functions
+
+  //returns top of stack
+  function peek() {
+    return stack.at(-1)
+  }
+
+  function addToOutput(token) {
+    output += " " + token
+  }
+
+  function handlePop() {
+    return stack.pop()
+  }
+
+  function handleToken(token) {
+    switch (true) {
+      // if it is a number, add it to output
+      case !isNaN(parseInt(token)):
+        addToOutput(token)
+        break
+
+      case opSymbols.includes(token):
+        const o1 = token
+        let o2 = peek()
+
+        while (
+          o2 !== undefined &&
+          o2 !== "(" &&
+          (operators[o2].prec > operators[o1].prec ||
+            (operators[o2].prec === operators[o1].prec &&
+              operators[o1].assoc === "left"))
+        ) {
+          addToOutput(handlePop())
+          o2 = peek()
+        }
+        stack.push(o1)
+        break
+      case token === "(":
+        stack.push(token)
+        break
+      case token === ")":
+        let topOfStack = peek()
+        while (topOfStack !== "(") {
+          assert(stack.length !== 0)
+          addToOutput(handlePop())
+          topOfStack = peek()
+        }
+        assert(peek() === "(")
+        handlePop()
+        break
+      default:
+        throw new Error(`Invalid token: ${token}`)
+    }
+  }
+  for (let i of input) {
+    if (i === " ") continue
 
     handleToken(i)
   }
 
+  while (stack.length !== 0) {
+    assert(peek() !== "(")
+    addToOutput(stack.pop())
+  }
+
   return output
 }
-
-function handleToken(token) {
-  const o1 = token
-  let o2 = stack.at(-1)
-  switch (true) {
-    case !isNan(parseInt(token)):
-      addToOutput(tokens)
-      break
-    case Object.keys(operators).includes(token):
-      while (
-        o2 !== undefined &&
-        o2 !== "(" &&
-        (operators[o2].prec > operators[o1].prec ||
-          (operators[o2].prec === operators[o1].prec &&
-            operators[o1].assoc === left))
-      ) {
-        addToOutput(stack.pop())
-        o2 = stack.at(-1)
-      }
-      stack.push(o1)
-      break
-    case token === "(":
-      stack.push(token)
-      break
-    case token === ")":
-      while (o2 !== "(") assert(stack.length !== 0)
-  }
-}
-
-function assert(predicate) {
-  if (predicate) return
-  throw new Error("Assertion failed due to invalid")
-}
-
-function addToOutput(addition) {
-  output += " " + token
-}
-
-// function isInt(x) {
-//   return !isNaN(parseInt(x))
-// }
-
-// function isValidOperator(x) {
-//   const operators = ["+", "-", "*", "/", "^", "(", ")"]
-// }
+const input = "1 + 2 * 3 - 4"
+const result = shuntingYard(input)
+console.log(result)
